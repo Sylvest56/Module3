81df47f40c205595819d8ad3d57a2c6b6a38df4c
Sylvester <106215759+Sylvest56@users.noreply.github.com>
2023-08-08 06:15:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const menuButton = document.querySelector('.menu-button');
-const dropdownMenu = document.querySelector('.dropdown-menu');
 
-menuButton.addEventListener('click', () => {
-    dropdownMenu.classList.toggle('show');
-});
